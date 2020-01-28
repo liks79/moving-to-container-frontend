6203309fcac1e2040879f561dfd5e1d32e13dcd5
Sungshik Jou <liks79@gmail.com>
2020-01-28 13:44:44 +0900

    Frontend initial version

---
+++ cloudalbum/src/service/photo/index.js
@@ -0,0 +1,15 @@
+import fileUpload from '@/service/photo/FileUpload';
+import fileUploadBase64 from '@/service/photo/FileUploadBase64';
+import gpsConverter from '@/service/photo/GpsConverter';
+import photoList from '@/service/photo/PhotoList';
+import deletePhoto from '@/service/photo/DeletePhoto';
+import getPhotoBlob from '@/service/photo/GetPhotoBlob';
+
+export default {
+  fileUpload,
+  fileUploadBase64,
+  gpsConverter,
+  photoList,
+  deletePhoto,
+  getPhotoBlob,
+};
